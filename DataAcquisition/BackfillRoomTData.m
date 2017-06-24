cf2f587f13c9038082abf7a9f2cb4ae1a273dc76
pennythewho <pennythewho@gmail.com>
2017-06-23 23:08:40 -0700

    set up loops to process experiment by experiment, day by day, from latest to oldest data

--- DataAcquisition/BackfillRoomTData.m
+++ DataAcquisition/BackfillRoomTData.m
@@ -5,7 +5,7 @@ earliestExp = datetime(2017,1,1);
 ultraSonicDataDir = '/Users/pennyespinoza/iSchool/BrownLab/Data/Ultrasonic Data/';
 
 % check that folder exists
-assert(exist(ultraSonicDataDir,'dir')==7, 'The UltraSonicData directory does not exist');
+assert(exist(replace(ultraSonicDataDir,'%20',' '),'dir')==7, 'The UltraSonicData directory does not exist');
     
 % get list of subdirs
 yyyymmddRE = '201[4-7][01][0-9][0-3][0-9]';
@@ -13,19 +13,27 @@ yyyymmddFmt = 'yyyyMMdd';
 exps = dir(replace(ultraSonicDataDir,'%20',' '));
 
 % add a new field with the date for each subdir, if any
-expDates = num2cell(datetime(cellfun(@(c)(regexp(c,strcat('^',yyyymmddRE),'match','once')),...
-    {exps.name},'UniformOutput',0),'InputFormat',yyyymmddFmt));
+expDates = datetime(cellfun(@(c)(regexp(c,strcat('^',yyyymmddRE),'match','once')),...
+    {exps.name},'UniformOutput',0),'InputFormat',yyyymmddFmt);
+expDates = num2cell(expDates);
 [exps(:).expStartDate] = expDates{:};
 
-% toss directories for experiments predating earliestExp or that have no
-% date
+% toss directories for experiments predating earliestExp or that have no date
 exps = exps([exps.isdir]==1 & ~isnat([exps.expStartDate]) & [exps.expStartDate]>=earliestExp(1));
     
-% order directories with latest first, oldest last
-
-% for each directory
+% for each directory (latest first, oldest last)
+specTimeFmt = 'M/d/yyyy h:mm:ss a';
+specDateStr = 'yyyymmdd';
+for ed = sort([exps.expStartDate],'descend')
+    exp = exps([exps.expStartDate]==ed);
+    disp(['Backfilling data for experiment ' exp.name]);
     % load UltraSonicStrc database
-    % for each distinct date
+    db = load(strcat(ultraSonicDataDir,exp.name,'/',exp.name,'.mat'));
+    % get distinct dates
+    specDates = sort(unique(datetime(datestr(datetime({db.data.SpectraTime},'InputFormat',specTimeFmt),specDateStr),'InputFormat',yyyymmddFmt)),'descend');
+    % for each distinct date (latest to oldest)
+    for d = specDates'
+        disp(['Loading CH3 data for ' datestr(d,specDateStr)]);
         % import/load CH3 data for the date
         % for each spectra on that date (latest to oldest)
             % if RoomT is empty
@@ -36,5 +44,7 @@ exps = exps([exps.isdir]==1 & ~isnat([exps.expStartDate]) & [exps.expStartDate]>
                 % fit log data
                 % update database RoomT and RoomTSTD
                 % save database
+    end
+end
     
 

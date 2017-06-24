f4e2ad9bff98e1c37e895c52d311c399635d9f0e
pennythewho <pennythewho@gmail.com>
2017-06-23 20:42:52 -0700

    load experiment dirs and ignore old ones or ones without dates

--- DataAcquisition/BackfillRoomTData.m
+++ DataAcquisition/BackfillRoomTData.m
@@ -1,26 +1,40 @@
 % Backfill Room Temperature data
 
 % parameters
-earliestExp = '2017/01/01';
-ultraSonicDataDir = '/Users/penny/Documents/iSchool/BrownLab/Data/UltrasonicData';
+earliestExp = datetime(2017,1,1);
+ultraSonicDataDir = '/Users/pennyespinoza/iSchool/BrownLab/Data/Ultrasonic Data/';
 
 % check that folder exists
+assert(exist(ultraSonicDataDir,'dir')==7, 'The UltraSonicData directory does not exist');
+    
 % get list of subdirs
-% get date for each subdir
-% toss directories for experiments predating earliestExp
+yyyymmddRE = '201[4-7][01][0-9][0-3][0-9]';
+yyyymmddFmt = 'yyyyMMdd';
+exps = dir(replace(ultraSonicDataDir,'%20',' '));
+
+% add a new field with the date for each subdir, if any
+expDates = num2cell(datetime(cellfun(@(c)(regexp(c,strcat('^',yyyymmddRE),'match','once')),...
+    {exps.name},'UniformOutput',0),'InputFormat',yyyymmddFmt));
+[exps(:).expStartDate] = expDates{:};
+
+% toss directories for experiments predating earliestExp or that have no
+% date
+exps = exps([exps.isdir]==1 & ~isnat([exps.expStartDate]) & [exps.expStartDate]>=earliestExp(1));
+    
 % order directories with latest first, oldest last
 
 % for each directory
     % load UltraSonicStrc database
-    % for each spectra in database
-        % if RoomT is empty
-            % determine date/time for spectra
-            % import/load CH3 data for the date
-            % select range of log entries surrounding the spectra
-            % discard zeros
-            % calculate RoomT average
-            % fit log data
-            % update database RoomT and RoomTSTD
-            % save database
+    % for each distinct date
+        % import/load CH3 data for the date
+        % for each spectra on that date (latest to oldest)
+            % if RoomT is empty
+                % determine date/time for spectra
+                % select range of log entries surrounding the spectra
+                % discard zeros
+                % calculate RoomT average
+                % fit log data
+                % update database RoomT and RoomTSTD
+                % save database
     
 

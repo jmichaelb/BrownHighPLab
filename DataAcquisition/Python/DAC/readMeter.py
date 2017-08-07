bb339c09551db1ab6ad34294916523e14e1fa445
pennythewho <pennespinoza+git@gmail.com>
2017-08-06 19:14:24 -0700

    streamline imports, add error handling, consistent var names

--- DataAcquisition/Python/DAC/readMeter.py
+++ DataAcquisition/Python/DAC/readMeter.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import pycurl, cStringIO, os, sys, getopt, re, time
+import pycurl, cStringIO, os, sys
+from re import search
+from getopt import getopt
+from datetime import datetime
+
+verbose = False
 
 
 def getReading(url, device, timeout_ms=1000):
@@ -9,23 +14,27 @@ def getReading(url, device, timeout_ms=1000):
     :param url: the URL (with optional port) of the meter
     :param device: the device (reading command in the web UI) from which to read
     :param timeout_ms: the max time allowed for each meter read, in ms (default 1000)
-    :return: returns a 2-tuple containing the (1) time of the reading and
+    :return: returns a 2-tuple containing the (1) time of the reading as a datetime object and
         (2) a string with the raw output from the meter
     """
+    try:
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.POSTFIELDS, '*' + device + '\r')
+        c.setopt(c.TIMEOUT_MS, timeout_ms)
+        c.setopt(c.HTTPHEADER, ['Pragma: no-cache'])
+        buffer = cStringIO.StringIO()
+        c.setopt(c.WRITEDATA, buffer)
+        readTime = datetime.utcnow()
+        c.perform()
+        c.close()
+        rawReading = buffer.getvalue()
+        buffer.close()
+        return (readTime, rawReading)
+    except Exception as e:
+        if verbose:
+            print 'Failed to read data from meter at '+dt.datetime.now()+':\n\t'+e
 
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.POSTFIELDS, '*' + device + '\r')
-    c.setopt(c.TIMEOUT_MS, timeout_ms)
-    c.setopt(c.HTTPHEADER, ['Pragma: no-cache'])
-    buffer = cStringIO.StringIO()
-    c.setopt(c.WRITEDATA, buffer)
-    readTime = time.gmtime()
-    c.perform()
-    c.close()
-    rawReading = buffer.getvalue()
-    buffer.close()
-    return (readTime, rawReading)
 
 
 def parseReading(rawReading, extractPattern, targetGroup=0):
@@ -37,7 +46,7 @@ def parseReading(rawReading, extractPattern, targetGroup=0):
     :return: returns the extracted meter reading as a float
     """
 
-    meterRead = re.search(extractPattern, rawReading)
+    meterRead = search(extractPattern, rawReading)
     return float(meterRead.group(targetGroup))
 
 
@@ -64,31 +73,37 @@ def mkExperimentDir(parentPath, expId):
         os.mkdir(expDirStr)
     return expDirStr
 
-def getLogFileName(expDir, device, readingTime, prependDate):
+def getLogFileName(expDir, device, readTime, prependDate):
     """Returns the target file name for the log data
 
     :param expDir: the path in which data for the current experiment is logged
     :param device: the identifier for the device being read
-    :param readingTime: the UTC struct_time the reading was taken
+    :param readTime: the datetime object for the time the reading was taken
     :param prependDate: True to prepend YYYYMMDD to the beginning of the log file
     :return: the full path, including file name, to which data will be logged
     """
     logFileName = expDir+'/'
     if prependDate:
-        logFileName = logFileName + time.strftime('%Y%m%d', readingTime) + '_'
+        logFileName = logFileName + readTime.strftime('%Y%m%d') + '_'
     return logFileName+'DP41-'+device+'_Uncorrected.txt'
 
 
-def writeLog(logFile, readTime, reading):
-    readTimeStr = time.strftime('%Y-%m-%d %H:%M:%S UTC', readTime)
+def logReading(logFile, readTime, reading):
+    """Appends new reading to a tab-delimited log
+
+    :param logFile: full path of the log file (see getLogFileName)
+    :param readTime: datetime object with time of the reading being added
+    :param reading:
+    :return:
+    """
+    ll = readTime.strftime('%Y-%m-%d %H:%M:%S UTC')+'\t'+repr(reading)+'\n'
+    print ll
     try:
-        lf = open(logFile, 'a')
-        try:
-            lf.write(readTimeStr+'\t'+reading+'\n')
-        finally:
-            lf.close()
+        with open(logFile, 'w') as lf:
+            lf.write(ll)
     except:
-        global logFailure = logFailure + 1
+        if verbose:
+            print 'Adding new reading to log file failed at '+dt.datetime.now()
 
 
 def getParms(parms=sys.argv[1:]):
@@ -108,11 +123,14 @@ def getParms(parms=sys.argv[1:]):
             if used, option value string without spaces required
     -i      the read interval, in seconds (default 5)
             if used, option value integer required
+    -l      lookback interval for plotting readings, in hours (default 2)
+            float value is acceptable
     -d      when present, indicates that YYYYMMDD will be prepended to log file names (default False)
             no option value supported
+    -v      verbose output (this will NOT output readings but it will no longer fail silently)
 
     :param parms: the list of parameters passed to this script.  Defaults to sys.arg[1:]
-    :return:
+    :return: a dictionary with output parameters, including defaults
     """
 
     # set up defaults
@@ -120,8 +138,9 @@ def getParms(parms=sys.argv[1:]):
            'logDir': os.getcwd(),
            'devId': 'X01',
            'readInt': 5,
+           'plotLookback': 2.0,
            'lfIncludesDate': False}
-    opts, args = getopt.getopt(parms, 'x:u:r:i:d')
+    opts, args = getopt(parms, 'x:p:u:r:i:l:dv')
     for opt, arg in opts:
         if opt == '-x':
             out['expId'] = arg
@@ -133,8 +152,13 @@ def getParms(parms=sys.argv[1:]):
             out['devId'] = arg
         elif opt == '-i':
             out['readInt'] = int(arg)
+        elif opt == '-l':
+            out['lookback'] = float(arg)
         elif opt == '-d':
             out['lfIncludesDate'] = True
+        elif opt == '-v':
+            global verbose
+            verbose = True
     return out
 
 def main():
@@ -143,20 +167,17 @@ def main():
         raise ValueError('You must provide an experiment id (-x parameter).')
     devPattern = getParsePattern(parms['devId'])
     expDir = mkExperimentDir(parms['logDir'], parms['expId'])
-    global logFailure = 0
 
     try :
         while true:
             (readTime, rawRead) = getReading(parms['url'], parms['devId'])
             reading = parseReading(rawRead, devPattern)
             logFileName = getLogFileName(expDir, parms['devId'], readTime, parms['lfIncludesDate'])
-            writeLog(logFileName, readTime, reading)
-            # plot reading
+            logReading(logFileName, readTime, reading)
+            # update plot
             time.sleep(parms['readInt'])
     except:
         # close files
         # close plots
-
-
-
+        print "error"
 

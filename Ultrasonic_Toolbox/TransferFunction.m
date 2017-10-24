6aff03852ac5454413f68b2140de42c2e75e7b02
pennythewho <pennespinoza+git@gmail.com>
2017-10-24 11:15:48 -0700

    rename myplot to plotSelect

--- Ultrasonic_Toolbox/TransferFunction.m
+++ Ultrasonic_Toolbox/TransferFunction.m
@@ -63,7 +63,7 @@ TF=TF/max(abs(TF));
        scrsz=get(0,'ScreenSize');
        figsz=[1 .8*scrsz(4) 3*scrsz(3)/4 .8*scrsz(4)];
         figure1=figure('Name','Click on Data Peaks','Position',figsz);
-        pks=smartPlot(UltraSonicStrc.t,TF);
+        pks=plotSelect(UltraSonicStrc.t,TF);
         close(figure1)
      case 'e'
         pks=UltraSonicStrc.pks;
